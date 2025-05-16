897d9fb10136fa56d2792eb793f0e44fa11d7df3
Milan <djordjevic.milan13@gmail.com>
2025-05-16 23:40:12 +0200

    og-image changing dimensions

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,8 +18,8 @@ export const metadata: Metadata = {
     images: [
       {
         url: '/og-image.png',
-        width: 1200,
-        height: 630,
+        width: 1024,
+        height: 1024,
         alt: 'Tipsy Trials Game Banner',
       },
     ],
